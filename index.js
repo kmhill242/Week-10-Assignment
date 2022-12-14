f3fe25034eba0d19e9540d127fb4f02b9adf6bb1
kmhill242 <kmhill242@gmail.com>
2022-12-14 14:54:47 -0500

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 let id = 0;
 
 document.getElementById('add').addEventListener('click', () => [
-let createdDate = new Date();
+    let createdDate = new Date();
 let table = document.getElementById('list');
 let row = table.insertRow(1);
 row.setAttribute('id', `item-$[id}`);
